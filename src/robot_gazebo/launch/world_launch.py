65708335c21d663337c6bab62578da7dc9ee8d34
ItzCobaltboy <pathanaffan62@gmail.com>
2024-12-04 18:07:57 +0530

    Update world_launch.py

--- src/robot_gazebo/launch/world_launch.py
+++ src/robot_gazebo/launch/world_launch.py
@@ -8,40 +8,47 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    # Default world
     pkg_dir = get_package_share_directory('robot_gazebo')
     default_world = os.path.join(pkg_dir, 'world', 'empty.world')
 
-    # Declare the 'world' launch argument with a default value
-    world_arg = DeclareLaunchArgument('world',default_value= default_world)
-
-    # Get the value of the 'world' argument
+    world_arg = DeclareLaunchArgument('world', default_value=default_world)
     world_file = LaunchConfiguration('world')
 
-    # Gazebo directory
     gazebo_ros_dir = get_package_share_directory('gazebo_ros')
 
-    # Define the Gazebo process
     gazebo_process = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(gazebo_ros_dir, 'launch', 'gazebo.launch.py')
-            ),
-            launch_arguments={'world': world_file}.items()
-        )
+        PythonLaunchDescriptionSource(
+            os.path.join(gazebo_ros_dir, 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments={'world': world_file}.items()
+    )
     
-    # Map Merger
+    robot_names = ['robot_1', 'robot_2']    # CHANGE FOR ALL ROBOTS
+
     merger_node = Node(
         package='robot_gazebo',
         executable='map_merger',
         parameters=[
-            {'use_sim_time': True}
+            {'use_sim_time': True},
+            {'robot_names': robot_names}
         ],
         output='screen',
     )
 
-    # Return the launch description
+    master_controller_launch = Node(
+        package='robot_gazebo', 
+        executable='master_controller', 
+        name='master_controller',
+        output='screen',
+        parameters=[{
+            'robot_names': robot_names
+        }],
+        remappings=[('/target', '/target')] 
+    )
+
     return LaunchDescription([
         world_arg,
         gazebo_process,
-        merger_node
+        merger_node,
+        master_controller_launch
     ])
